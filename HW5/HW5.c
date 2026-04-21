12c7309ba0a291c343c221968408fb50b04cbee4
Melite12 <Manash.dugar@gmail.com>
2026-04-20 20:19:57 -0500

    LED + constants

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -2,30 +2,43 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
-
+#include "HW5.h"
+#include "pico/cyw43_arch.h"
 
 
 int main()
 {
     stdio_init_all();
+    i2c_init_all();
+    int rc = cyw43_arch_init();
+    int led = 1;
+
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led);
+        led = !led;
+        sleep_ms(500);
+    }
+}
+
+void i2c_init_all(){
 
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
+
+    i2c_init(I2C_PORT_1, 400*1000);
+    
+    gpio_set_function(I2C_SDA_1, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_1, GPIO_FUNC_I2C);
+
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    // i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+    // Addr = 7 bit address of the chip 
+    // buf = array of 8bit data you are sending
+    // 2 = length of array (should math buf?)
+    // false = write ; true = read
+    // first byte of buf = address 
 }
---
+++ HW5/HW5.h
@@ -0,0 +1,44 @@
+#ifndef HW5_H
+#define HW5_H
+
+// i2c for screen
+#define I2C_PORT i2c0
+#define I2C_SDA 4
+#define I2C_SCL 5
+
+//i2c for imu
+#define I2C_PORT_1 i2c1
+#define I2C_SDA_1 14
+#define I2C_SCL_1 15
+
+// config / misc
+#define ADDR 0x68;
+#define CONFIG 0x1A
+#define GYRO_CONFIG 0x1B
+#define ACCEL_CONFIG 0x1C
+#define PWR_MGMT_1 0x6B
+#define PWR_MGMT_2 0x6C
+#define WHO_AM_I 0x75;
+
+// sensor data registers
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_XOUT_L 0x3C
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_YOUT_L 0x3E
+#define ACCEL_ZOUT_H 0x3F
+#define ACCEL_ZOUT_L 0x40
+#define TEMP_OUT_H   0x41
+#define TEMP_OUT_L   0x42
+#define GYRO_XOUT_H  0x43
+#define GYRO_XOUT_L  0x44
+#define GYRO_YOUT_H  0x45
+#define GYRO_YOUT_L  0x46
+#define GYRO_ZOUT_H  0x47
+#define GYRO_ZOUT_L  0x48
+
+
+
+
+void i2c_init_all();
+
+#endif
