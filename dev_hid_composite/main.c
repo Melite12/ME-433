50a1b3a14e8f1d32d01880c4227a8cf53417e290
Melite12 <Manash.dugar@gmail.com>
2026-04-22 13:53:55 -0500

    not working :(

--- dev_hid_composite/main.c
+++ dev_hid_composite/main.c
@@ -32,6 +32,14 @@
 
 #include "usb_descriptors.h"
 
+#include "main.h"
+#include "ssd1306.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "pico/cyw43_arch.h"
+
+#define BUTTON 10
+
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
 //--------------------------------------------------------------------+
@@ -56,6 +64,11 @@ void hid_task(void);
 int main(void)
 {
   board_init();
+  button_init();
+  led_init();
+    int led = 1;
+  imu_init();
+    struct Data data;
 
   // init device stack on configured roothub port
   tud_init(BOARD_TUD_RHPORT);
@@ -65,11 +78,25 @@ int main(void)
   }
 
   while (1)
-  {
-    tud_task(); // tinyusb device task
-    led_blinking_task();
+  { 
+    if(gpio_get(BUTTON) == 0){
+      led = !led;
+      cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led);
+    }
 
-    hid_task();
+    data = readData(ADDR, ACCEL_XOUT_H);
+
+    // printf("Acc_x: %.3f   Acc_y: %.3f   Acc_z: %.3f\n", data.acc_x, data.acc_y, data.acc_z);
+    // printf("Gyro_x: %.3f   Gryo_y: %.3f   Gyro_z: %.3f\n", data.gyro_x, data.gyro_y, data.gyro_z);
+    // printf("Temp: %.2f\n", data.temp);
+
+    drawScreen(data);
+
+    sleep_ms(1000);
+    //tud_task(); // tinyusb device task
+    //led_blinking_task();
+
+    //hid_task();
   }
 }
 
@@ -142,7 +169,7 @@ static void send_hid_report(uint8_t report_id, uint32_t btn)
 
       // no button, right + down, no scroll, no pan
       tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, delta, delta, 0, 0);
-    }
+    } 
     break;
 
     case REPORT_ID_CONSUMER_CONTROL:
@@ -220,7 +247,7 @@ void hid_task(void)
   }else
   {
     // Send the 1st of report chain, the rest will be sent by tud_hid_report_complete_cb()
-    send_hid_report(REPORT_ID_MOUSE, btn);
+    send_hid_report(REPORT_ID_MOUSE, btn); // was REPORT_ID_KEYBOARD
   }
 }
 
@@ -303,4 +330,126 @@ void led_blinking_task(void)
 
   board_led_write(led_state);
   led_state = 1 - led_state; // toggle
+}
+
+
+
+// MY FUNCTIONS
+
+void i2c_init_all(){
+
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400*1000);
+    
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+
+    i2c_init(I2C_PORT_1, 400*1000);
+    
+    gpio_set_function(I2C_SDA_1, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_1, GPIO_FUNC_I2C);
+
+    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    // i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+    // Addr = 7 bit address of the chip 
+    // buf = array of 8bit data you are sending
+    // 2 = length of array (should math buf?)
+    // false = write ; true = read
+    // first byte of buf = address 
+}
+
+void writePin(unsigned char addr, unsigned char reg, unsigned char val){
+
+    uint8_t buf[2] = {reg, val};
+    i2c_write_blocking(I2C_PORT_1, addr, buf, 2, false);
+
+}
+
+unsigned char readPin(unsigned char addr, unsigned char reg){
+    
+    unsigned char readbuf;
+    uint8_t buf[1] = {reg};
+
+    i2c_write_blocking(I2C_PORT_1, addr, buf, 1, true);  // true to keep host control of bus
+    i2c_read_blocking(I2C_PORT_1, addr, &readbuf, 1, false);  // false - finished with bus
+
+    return readbuf;
+}
+
+struct Data readData(unsigned char addr, unsigned char reg){
+    
+    unsigned char readbuf[14];
+    uint8_t buf[1] = {reg};
+
+    int wr = i2c_write_blocking(I2C_PORT_1, addr, buf, 1, true);  // true to keep host control of bus
+    int rd = i2c_read_blocking(I2C_PORT_1, addr, readbuf, 14, false);  // false - finished with bus
+
+    if (wr == PICO_ERROR_GENERIC || rd == PICO_ERROR_GENERIC) {
+        printf("I2C error! wr=%d rd=%d\n", wr, rd);
+        // return empty/last data or handle error
+    }
+
+    struct Data data;
+    data.acc_x =  (int16_t)((readbuf[0] << 8) | readbuf[1]) * ACC_TO_G ;
+    data.acc_y =  (int16_t)((readbuf[2] << 8) | readbuf[3]) * ACC_TO_G;
+    data.acc_z =  (int16_t)((readbuf[4] << 8) | readbuf[5]) * ACC_TO_G;
+    data.temp =   (int16_t)((readbuf[6] << 8) | readbuf[7]) / 340.0 + 36.53;
+    data.gyro_x = (int16_t)((readbuf[8] << 8) | readbuf[9]) * GYRO_TO_DPS;
+    data.gyro_y = (int16_t)((readbuf[10] << 8) | readbuf[11]) * GYRO_TO_DPS;
+    data.gyro_z = (int16_t)((readbuf[12] << 8) | readbuf[13]) * GYRO_TO_DPS;
+
+    return data;
+}
+
+void button_init(){
+  gpio_init(BUTTON);
+  gpio_set_dir(BUTTON, GPIO_IN); 
+  gpio_pull_up(BUTTON);
+}
+
+void led_init(){
+  int rc = cyw43_arch_init();
+  cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+}
+
+void imu_init(){
+  sleep_ms(3000);
+  unsigned char testing = readPin(ADDR, WHO_AM_I);
+  //printf("%d",testing);
+
+  writePin(ADDR, PWR_MGMT_1, 0x00);
+  writePin(ADDR, ACCEL_CONFIG, 0x00);
+  writePin(ADDR, GYRO_CONFIG, 0b00011000);
+}
+
+void drawScreen(struct Data data){
+  int x_val = (int)((data.acc_x) * 64);
+  int y_val = (int)((data.acc_y) * 16);
+
+  ssd1306_clear();
+  
+  if (x_val > 0){
+      for (int i = 0; i < x_val; i++){
+          ssd1306_drawPixel(64 - i, 16, 1);
+      }
+  }
+  else{
+      for (int i = 0; i < abs(x_val); i++){
+          ssd1306_drawPixel(64 + i, 16, 1);
+      }
+  }
+
+  if (y_val > 0){
+      for (int i = 0; i < y_val; i++){
+          ssd1306_drawPixel(64, i + 16, 1);
+      }
+  }
+  else{
+      for (int i = 0; i < abs(y_val); i++){
+          ssd1306_drawPixel(64, 16 - i, 1);
+      }
+  }
+
+  ssd1306_update();
 }
---
+++ dev_hid_composite/main.h
@@ -0,0 +1,64 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// i2c for screen
+#define I2C_PORT i2c0
+#define I2C_SDA 4
+#define I2C_SCL 5
+
+//i2c for imu
+#define I2C_PORT_1 i2c1
+#define I2C_SDA_1 14
+#define I2C_SCL_1 15
+
+// config / misc
+#define ADDR 0x68
+#define CONFIG 0x1A
+#define GYRO_CONFIG 0x1B
+#define ACCEL_CONFIG 0x1C
+#define PWR_MGMT_1 0x6B
+#define PWR_MGMT_2 0x6C
+#define WHO_AM_I 0x75
+
+// sensor data registers
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_XOUT_L 0x3C
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_YOUT_L 0x3E
+#define ACCEL_ZOUT_H 0x3F
+#define ACCEL_ZOUT_L 0x40
+#define TEMP_OUT_H   0x41
+#define TEMP_OUT_L   0x42
+#define GYRO_XOUT_H  0x43
+#define GYRO_XOUT_L  0x44
+#define GYRO_YOUT_H  0x45
+#define GYRO_YOUT_L  0x46
+#define GYRO_ZOUT_H  0x47
+#define GYRO_ZOUT_L  0x48
+
+// Conversion
+#define ACC_TO_G 0.000061
+#define GYRO_TO_DPS 0.007630
+
+struct Data{
+    float acc_x;
+    float acc_y;
+    float acc_z;
+    float temp;
+    float gyro_x;
+    float gyro_y;
+    float gyro_z;
+};
+
+void i2c_init_all();
+void button_init();
+void led_init();
+void imu_init();
+
+unsigned char readPin(unsigned char addr, unsigned char reg);
+void writePin(unsigned char addr, unsigned char reg, unsigned char val);
+
+struct Data readData(unsigned char addr, unsigned char reg);
+void drawScreen(struct Data data);
+
+#endif
