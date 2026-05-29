b94049ae9b0b17641385a674dd233b0ba9dc3988
Melite12 <Manash.dugar@gmail.com>
2026-05-29 16:48:13 -0500

    HW14_init

---
+++ HW14/HW14.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "HW14.h"
+
+
+int main()
+{
+    stdio_init_all();
+
+    hx711_init();
+
+    while (true) {
+        int num;
+        scanf("%d",&num);
+
+        int voltages[1000];
+        uint64_t times[1000];
+
+        for (int i = 0; i < num; i++){
+            int val = hx711_read();
+
+            // any filtering goes here
+
+            voltages[i] = val;
+            times[i] = to_ms_since_boot(get_absolute_time());
+        }
+
+        for (int i = 0; i < num; i++){
+            printf("%d %llu %d", i, times[i], voltages[i]);
+        }
+
+        sleep_ms(100);
+    }
+}
+
+void hx711_init(){
+    // Not too sure why there's a pull up for PIN_DOUT (does it stay pulled up)
+
+    gpio_init(PIN_DOUT);
+    gpio_set_dir(PIN_DOUT, GPIO_IN);
+    gpio_pull_up(PIN_DOUT);
+
+    gpio_init(PIN_SCK);
+    gpio_set_dir(PIN_SCK, GPIO_OUT);
+    gpio_put(PIN_SCK, 0);
+}
+
+int hx711_read(){
+
+    uint64_t clock_time_us = 10;
+    unsigned int data;
+
+    while(gpio_get(PIN_DOUT)){
+        tight_loop_contents();
+    }
+
+    unsigned int raw;
+    for (int i = 0; i < 24; i++){
+        gpio_put(PIN_SCK, 1);
+        sleep_us(clock_time_us);
+
+        if (gpio_get(PIN_DOUT)){
+            data = 1;
+        }
+        else{
+            data = 0;
+        }
+
+        raw = (raw << 1) | data;
+
+        gpio_put(PIN_SCK, 0);
+        sleep_us(clock_time_us);
+    }
+
+    // set gain for next piece of data
+    gpio_put(PIN_SCK, 1);
+    sleep_us(clock_time_us);
+    gpio_put(PIN_SCK, 0);
+    sleep_us(clock_time_us);
+
+    if (raw & 0x800000){
+        raw = raw | 0xFF000000;
+    }
+
+    return (int)raw;
+}
---
+++ HW14/HW14.h
@@ -0,0 +1,10 @@
+#ifndef HW14_H
+#define HW14_H
+
+#define PIN_DOUT 10
+#define PIN_SCK 11
+
+void hx711_init();
+int hx711_read();
+
+#endif
